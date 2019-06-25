37229e655ba855e898274c1262c866fdf2a092ea
Isaque <isaquegalaxy@gmail.com>
2019-06-25 00:17:29 -0300

    Exercicio Geografia

---
+++ src/entities/Cidade.java
@@ -0,0 +1,17 @@
+package entities;
+
+import java.util.List;
+
+public class Cidades {
+    private String name;
+    private String estado;
+    private Ordem ordem;
+
+    public Cidades(String name, String estado, Ordem ordem) {
+        this.name = name;
+        this.estado = estado;
+        this.ordem = ordem;
+    }
+
+    List<Cidades>cidades
+}
